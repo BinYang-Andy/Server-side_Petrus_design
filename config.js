fdd20cc6d1233778c16982e0c38ce450d098f8c5
Bin Yang <magic511865789@hotmail.com>
2021-12-16 11:58:11 -0500

    OAuth 2 authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl':'mongodb://localhost:27017/conFusion'
+    'mongoUrl':'mongodb://localhost:27017/conFusion',
+    'facebook':{
+        clientId: '273946974763476',
+        clientSecret: '68e83197465d3ec80e9f543ca2c1cfe2'
+    }
 }
